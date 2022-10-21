8880673706f3df17d1a21a2ecb2565b3911494f0
Chris Nevin <chris.nevin@tigerspike.com>
2022-10-21 12:17:24 +1000

    Initial release.

--- EmptiableTypes/README.md
+++
@@ -1,3 +0,0 @@
-# EmptiableTypes
-
-A description of this package.
--- EmptiableTypes/Sources/EmptiableTypes/EmptiableTypes.swift
+++
@@ -1,6 +0,0 @@
-public struct EmptiableTypes {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
